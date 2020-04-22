26c18a2341c3c6f6a0d0dc17db54f8bbfe5bcf6d
Jason Paige <jason.r.paige@gmail.com>
2020-04-22 14:27:14 +0100

    Feature toggle and tf 0.12

--- variables.tf
+++ variables.tf
@@ -23,3 +23,9 @@ variable "account_replication_type" {
   default     = "LRS"
   description = "Account Replication Type for the Azure Media Services instance. Defaults to Locally Redundant"
 }
+
+variable "enabled" {
+  default     = true
+  type        = bool
+  description = "Feature toggle flag to enable/disable the module in certain circumstances"
+}
