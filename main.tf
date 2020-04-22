d9ff38b0a67c30df0e8328fdd22bdf0fe66cf50b
Jason Paige <jason.r.paige@gmail.com>
2020-04-22 14:56:32 +0100

    Fixes for 0.12

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "azurerm" {
+  version = "=2.3.0"
+  features {}
+}
+
 locals {
   rg_name      = "${var.product}-media-service-${var.env}"
   sa_name      = "${var.product}mediaservice${var.env}"
@@ -14,8 +19,8 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_storage_account" "sa" {
   count                    = var.enabled ? 1 : 0
   name                     = local.sa_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.0.name
+  location                 = azurerm_resource_group.rg.0.location
   tags                     = var.common_tags
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
@@ -24,12 +29,11 @@ resource "azurerm_storage_account" "sa" {
 resource "azurerm_media_services_account" "media-service" {
   count               = var.enabled ? 1 : 0
   name                = local.service_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  tags                = var.common_tags
+  resource_group_name = azurerm_resource_group.rg.0.name
+  location            = azurerm_resource_group.rg.0.location
 
   storage_account {
-    id         = azurerm_storage_account.sa.id
+    id         = azurerm_storage_account.sa.0.id
     is_primary = true
   }
 }
