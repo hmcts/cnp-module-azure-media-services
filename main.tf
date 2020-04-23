e959175469663c57f7805156254cff512db011ae
Jason Paige <jasonpaige@users.noreply.github.com>
2020-04-23 21:19:16 +0100

    Media service name must be alphanumeric (#4)

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 locals {
   rg_name      = "${var.product}-media-service-${var.env}"
   sa_name      = "${var.product}mediaservice${var.env}"
-  service_name = "${var.product}-media-service-${var.env}"
+  service_name = "${var.product}mediaservice${var.env}"
 }
 
 resource "azurerm_resource_group" "rg" {
